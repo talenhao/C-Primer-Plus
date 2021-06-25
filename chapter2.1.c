58a9cdd851fbbdcf34edc4d122c69456f5b5342d
Tianfei Hao <haotianfei@163.com>
2021-06-25 10:38:41 +0800

    chapter 4

---
+++ chapter2.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  /* 预处理指令。*/
+int main(void){     /* 返回值类型 main（参数void表示空参）函数是起点。 */
+    int num;        /* 声明 */
+    num = 1;        /* 赋值语句 */
+    printf("I'm a sample ");
+    printf("computer.\n");
+    printf("My favorite number is %d because it is first.", num);
+
+    return 0;
+}
