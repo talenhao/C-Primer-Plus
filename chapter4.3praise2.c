be9307649ccc08ab39f61588d65a40664d2108f5
Tianfei Hao <haotianfei@163.com>
2021-06-25 10:49:14 +0800

    sizeof use ()

--- chapter4.3praise2.c
+++ chapter4.3praise2.c
@@ -10,7 +10,7 @@ int main(void) {
     printf("What's your name? ");
     scanf("%s", name);
     printf("Hello, %s. %s\n", name, PRAISE);
-    printf("Your name of %zd letters occupies %zd memory cells.\n", strlen(name), sizeof name);
+    printf("Your name of %zd letters occupies %zd memory cells.\n", strlen(name), sizeof(name));
     printf("The phrase of praise has %zd letters ", strlen(PRAISE));
-    printf("and occupies %zd memory cells.\n", sizeof PRAISE);
+    printf("and occupies %zd memory cells.\n", sizeof(PRAISE));
 }
